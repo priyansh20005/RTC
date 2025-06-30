dbb182b9dc20c018d4ec0fb6fe4f3f5b19c15557
priyansh koshti <priyanshkos02@gmail.com>
2025-06-30 22:32:31 +0530

    web socket

---
+++ webSocket.js
@@ -0,0 +1,20 @@
+const WebSocket = require("ws");
+
+
+const server = new WebSocket.Server({port:3000});
+
+server.on("connection" , (socket)=>{
+    console.log("Client connected ");
+
+    socket.send("hello web socket  ");
+    socket.on("message" , (message)=>{
+        console.log(message);
+        socket.send("hello from server ");
+    });
+});
+
+// const socket = new WebSocket("ws://localhost:3000");
+
+// socket.onmessage = (event)=>{
+//     console.log(event.data);
+// }
